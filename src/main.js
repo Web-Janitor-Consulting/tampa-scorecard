cf024bb1de1621b9701b2474475e51da847c7a99
Michael Bishop <work@michaelbishop.me>
2023-09-05 00:48:57 -0400

    fix: load from external json file

--- src/main.js
+++ src/main.js
@@ -1,24 +1,31 @@
-// load the data from pci.json
-const cpiTPA = require('./pci.json');
+fetch('pci.json')
+    .then(response => response.json())
+    .then(data => {
+        const cpiTPA = data;
+        console.log(cpiTPA);
 
-const ctx = document.getElementById('pci');
+        const ctx = document.getElementById('pci');
 
-new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: cpiTPA.map(row => row.year),
-        datasets: [{
-            label: 'Tampa',
-            data: cpiTPA.map(row => row.amount),
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                min: 15000,
-                max: 50000,
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: cpiTPA.map(row => row.year),
+                datasets: [{
+                    label: 'Tampa',
+                    data: cpiTPA.map(row => row.amount),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        min: 15000,
+                        max: 50000,
+                    }
+                }
             }
-        }
-    }
-});
+        });
+    })
+    .catch(error => {
+        console.error('Error loading data:', error);
+    });
