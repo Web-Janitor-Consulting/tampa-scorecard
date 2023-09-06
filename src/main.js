30f09d0f4ac80fefb813ff811a23d0d9c5342508
Michael Bishop <work@michaelbishop.me>
2023-09-05 22:10:52 -0400

    feat: chartDeferred plugin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+Chart.register(ChartDeferred);
+
 fetch('pci.json')
     .then(response => response.json())
     .then(data => {
@@ -21,6 +23,11 @@ fetch('pci.json')
             data: {
                 labels: data[0].data.map(row => row.year),
                 datasets: datasets
+            },
+            plugins: {
+                deferred: {
+                    delay: 4000,
+                },
             },
             options: {
                 scales: {
@@ -61,6 +68,11 @@ fetch('pci-benchmark.json')
                 labels: data[0].data.map(row => row.year),
                 datasets: datasets
             },
+            plugins: {
+                deferred: {
+                    delay: 4000,
+                },
+            },
             options: {
                 scales: {
                     yAxes: [{
@@ -68,6 +80,58 @@ fetch('pci-benchmark.json')
                             beginAtZero: true
                         }
                     }]
+                }
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Error loading data:', error);
+    });
+
+fetch('high-earners.json')
+    .then(response => response.json())
+    .then(data => {
+        const ctx = document.getElementById('high-earners');
+        console.log(data);
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: data.map(row => row.city),
+                datasets: [{
+                    label: 'Percent of Population Earning $200,000 or more 2021',
+                    data: data.map(row => row.percentage),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            plugins: {
+                deferred: {
+                    delay: 4000,
+                },
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    }
                 }
             }
         });
