47a582a70de78b707e4597242602a43564009a18
Michael Bishop <work@michaelbishop.me>
2023-09-16 03:44:07 -0400

    feature: data tables generate tables from chart data for accessibility and tuck into a `<details>` element

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,48 @@
-const path = require("node:path");
+const path = require('path');
+
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/*.{css,jpg,json,svg}");
     eleventyConfig.addPassthroughCopy("./src/main.js");
+    eleventyConfig.addPassthroughCopy("./src/_data");
+    eleventyConfig.addShortcode('fl-line', function(metric) {
+        const tbody = [];
+        for (const { city, data }
+            of metric) {
+            const amounts = data
+                .map(({ amount }) => `<td>\$${ amount }</td>`)
+                .join("");
+            tbody.push(`<tr><td>${ city }</td>${ amounts }</tr>`);
+        }
+
+        return `<table>
+      <thead>
+        <tr>
+          <th>City</th>
+          <th>2012</th>
+          <th>2013</th>
+          <th>2014</th>
+          <th>2015</th>
+          <th>2016</th>
+          <th>2017</th>
+          <th>2018</th>
+          <th>2019</th>
+          <th>2020</th>
+          <th>2021</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tbody.join("\n")}
+      </tbody>
+    </table>`;
+
+    });
+
+
     return {
         dir: {
-            input: "src",
-        },
+            input: 'src',
+            output: '_site'
+        }
     };
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,124 @@
+//https://css-tricks.com/how-to-animate-the-details-element-using-waapi/
+
+class Accordion {
+    constructor(el) {
+        // Store the <details> element
+        this.el = el;
+        // Store the <summary> element
+        this.summary = el.querySelector('summary');
+        // Store the <div class="content"> element
+        this.content = el.querySelector('table');
+
+        // Store the animation object (so we can cancel it if needed)
+        this.animation = null;
+        // Store if the element is closing
+        this.isClosing = false;
+        // Store if the element is expanding
+        this.isExpanding = false;
+        // Detect user clicks on the summary element
+        this.summary.addEventListener('click', (e) => this.onClick(e));
+    }
+
+    onClick(e) {
+        // Stop default behaviour from the browser
+        e.preventDefault();
+        // Add an overflow on the <details> to avoid content overflowing
+        this.el.style.overflow = 'hidden';
+        // Check if the element is being closed or is already closed
+        if (this.isClosing || !this.el.open) {
+            this.open();
+            // Check if the element is being openned or is already open
+        } else if (this.isExpanding || this.el.open) {
+            this.shrink();
+        }
+    }
+
+    shrink() {
+        // Set the element as "being closed"
+        this.isClosing = true;
+
+        // Store the current height of the element
+        const startHeight = `${this.el.offsetHeight}px`;
+        // Calculate the height of the summary
+        const endHeight = `${this.summary.offsetHeight}px`;
+
+        // If there is already an animation running
+        if (this.animation) {
+            // Cancel the current animation
+            this.animation.cancel();
+        }
+
+        // Start a WAAPI animation
+        this.animation = this.el.animate({
+            // Set the keyframes from the startHeight to endHeight
+            height: [startHeight, endHeight]
+        }, {
+            duration: 400,
+            easing: 'ease-out'
+        });
+
+        // When the animation is complete, call onAnimationFinish()
+        this.animation.onfinish = () => this.onAnimationFinish(false);
+        // If the animation is cancelled, isClosing variable is set to false
+        this.animation.oncancel = () => this.isClosing = false;
+    }
+
+    open() {
+        // Apply a fixed height on the element
+        this.el.style.height = `${this.el.offsetHeight}px`;
+        // Force the [open] attribute on the details element
+        this.el.open = true;
+        // Wait for the next frame to call the expand function
+        window.requestAnimationFrame(() => this.expand());
+    }
+
+    expand() {
+        // Set the element as "being expanding"
+        this.isExpanding = true;
+        // Get the current fixed height of the element
+        const startHeight = `${this.el.offsetHeight}px`;
+        // Calculate the open height of the element (summary height + content height)
+        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+
+        // If there is already an animation running
+        if (this.animation) {
+            // Cancel the current animation
+            this.animation.cancel();
+        }
+
+        // Start a WAAPI animation
+        this.animation = this.el.animate({
+            // Set the keyframes from the startHeight to endHeight
+            height: [startHeight, endHeight]
+        }, {
+            duration: 400,
+            easing: 'ease-out'
+        });
+        // When the animation is complete, call onAnimationFinish()
+        this.animation.onfinish = () => this.onAnimationFinish(true);
+        // If the animation is cancelled, isExpanding variable is set to false
+        this.animation.oncancel = () => this.isExpanding = false;
+    }
+
+    onAnimationFinish(open) {
+        // Set the open attribute based on the parameter
+        this.el.open = open;
+        // Clear the stored animation
+        this.animation = null;
+        // Reset isClosing & isExpanding
+        this.isClosing = false;
+        this.isExpanding = false;
+        // Remove the overflow hidden and the fixed height
+        this.el.style.height = this.el.style.overflow = '';
+    }
+}
+
+document.querySelectorAll('details').forEach((el) => {
+    new Accordion(el);
+});
 Chart.register(ChartDeferred);
 
-fetch('pci.json')
+fetch('_data/pci.json')
     .then(response => response.json())
     .then(data => {
         const datasets = [];
@@ -26,7 +144,8 @@ fetch('pci.json')
             },
             plugins: {
                 deferred: {
-                    delay: 4000,
+                    yOffset: '75%',
+                    delay: 500
                 },
             },
             options: {
@@ -44,7 +163,7 @@ fetch('pci.json')
         console.error('Error loading data:', error);
     });
 
-fetch('pci-benchmark.json')
+fetch('_data/pci-benchmark.json')
     .then(response => response.json())
     .then(data => {
         const datasets = [];
@@ -70,7 +189,7 @@ fetch('pci-benchmark.json')
             },
             plugins: {
                 deferred: {
-                    delay: 4000,
+                    yOffset: '75%',
                 },
             },
             options: {
@@ -124,7 +243,8 @@ fetch('high-earners.json')
             },
             plugins: {
                 deferred: {
-                    delay: 4000,
+                    yOffset: '100%',
+                    delay: 3000
                 },
             },
             options: {
